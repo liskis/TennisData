8a79c87a1237047a06c6b017f2f07d9e370bcfec
liskis <watanabe@liskis.org>
2023-12-28 15:41:59 +0900

    ファイルを作成

--- TennisData4/ContentView.swift
+++ TennisData4/ContentView.swift
@@ -1,21 +1,132 @@
 //
 //  ContentView.swift
-//  TennisData4
+//  TennisData
 //
-//  Created by 渡辺健輔 on 2023/12/28.
+//  Created by 渡辺健輔 on 2023/12/27.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        let appearance: UITabBarAppearance = UITabBarAppearance()
+        appearance.backgroundColor = .black
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TabView {
+                HomeView().tabItem {
+                    Text("HOME")
+                    Image(systemName: "house.fill")
+                }
+                MatchRecordView().tabItem {
+                    Text("試合履歴")
+                    Image(systemName: "list.bullet")
+                }
+                DataAnalysisView().tabItem {
+                    Text("データ分析")
+                    Image(systemName: "chart.pie.fill")
+                }
+            }
+            .accentColor(.white)
+
+
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Text("").frame(height:1)
+                HStack {
+                    LogoImage()
+                    HStack(spacing:0) {
+                        UserNameBtn()
+                        LebelBtn()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                Image("background")
+                    .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fit)
+//                    .resizable()
+                    .scaledToFill()
+//                    .frame(maxWidth: .infinity,maxHeight:500)
+//                    .clipShape(Rectangle())
+            }
+            
         }
-        .padding()
+    }
+}
+
+struct LogoImage: View {
+    var body: some View {
+        Image("logo")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 120,height: 20, alignment: .leading)
+    }
+}
+
+struct UserNameBtn: View {
+    var body: some View {
+        Image("angleG")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 15,height: 30)
+        Button(action: {
+             //タップ処理
+        },label:  {
+            Text("渡辺健輔さん")
+                .font(.custom("", size: 10))
+                
+        })
+        .frame(width: 100, height:30)
+        .background(.gray)
+    }
+}
+struct LebelBtn: View {
+    var body: some View {
+        Image("angleGW")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 15,height: 30)
+        Button(action: {
+             //タップ処理
+        },label:  {
+            VStack {
+                Text("初心者")
+                    .font(.custom("", size: 8))
+                    .foregroundColor(Color.black)
+                Text("ビギナーモード")
+                    .font(.custom("", size: 8))
+                    .foregroundColor(Color.orange)
+            }
+        })
+        .frame(width: 80, height:30)
+        .background(.white)
+    }
+}
+
+struct MatchRecordView: View {
+    var body: some View {
+        ZStack {
+           
+           
+
+        }
+    }
+}
+
+struct DataAnalysisView: View {
+    var body: some View {
+        ZStack {
+           
+
+        }
     }
 }
 
